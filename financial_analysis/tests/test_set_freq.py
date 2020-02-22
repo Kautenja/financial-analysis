2092d2f8bff3ab9c30146fe376944717059f889b
Kautenja <kautencreations@gmail.com>
2020-02-22 15:35:02 -0600

    test lossless converion

--- financial_analysis/tests/test_set_freq.py
+++ financial_analysis/tests/test_set_freq.py
@@ -4,6 +4,46 @@ import numpy as np
 import pandas as pd
 from ..set_freq import set_freq
 
+#
+# MARK: lossless conversions
+#
+
+class ShouldLosslessConvertUsingGroupby(TestCase):
+    def test(self):
+        index = pd.to_datetime([1000, 2000, 3000], unit='ms')
+        price = pd.Series([100., 101., 102.], index=index)
+        dividend = pd.Series([5., 6., 7.], index=index)
+        price, dividend = set_freq(price, dividend, '1s', groupby=True)
+        # create the expected index and price
+        expected_index = pd.to_datetime([1, 2, 3], unit='s')
+        # expected values will have NaN where there were none
+        expected_price = pd.Series([100., 101., 102.], index=expected_index)
+        expected_dividend = pd.Series([5., 6., 7.], index=expected_index)
+        # make assertions through the pandas testing module
+        pd.testing.assert_series_equal(price, expected_price)
+        pd.testing.assert_series_equal(dividend, expected_dividend)
+
+
+class ShouldLosslessConvertUsingAsfreq(TestCase):
+    def test(self):
+        index = pd.to_datetime([1000, 2000, 3000], unit='ms')
+        price = pd.Series([100., 101., 102.], index=index)
+        dividend = pd.Series([5., 6., 7.], index=index)
+        price, dividend = set_freq(price, dividend, '1s', groupby=False)
+        # create the expected index and price
+        expected_index = pd.to_datetime([1, 2, 3], unit='s')
+        # expected values will have NaN where there were none
+        expected_price = pd.Series([100., 101., 102.], index=expected_index)
+        expected_dividend = pd.Series([5., 6., 7.], index=expected_index)
+        # make assertions through the pandas testing module
+        pd.testing.assert_series_equal(price, expected_price)
+        pd.testing.assert_series_equal(dividend, expected_dividend)
+
+
+#
+# MARK: Up-sampling (inserting missing values)
+#
+
 
 class ShouldUpsampleTimeScaleUsingFfillTrue(TestCase):
     def test(self):
@@ -35,3 +75,44 @@ class ShouldNotUpsampleTimeScaleUsingFfillFalse(TestCase):
         # make assertions through the pandas testing module
         pd.testing.assert_series_equal(price, expected_price)
         pd.testing.assert_series_equal(dividend, expected_dividend)
+
+#
+# MARK: Down-sampling (aggregating groups of data)
+#
+
+# class ShouldDownsampleUsingMeanValue(TestCase):
+#     def test(self):
+#         index = pd.to_datetime([1000, 2000], unit='ms')
+#         price = pd.Series([100., 101.], index=index)
+#         dividend = pd.Series([5., 6.], index=index)
+#         price, dividend = set_freq(price, dividend, '1T')
+#         print()
+#         print(price)
+#         print(dividend)
+#         # create the expected index and price
+#         expected_index = pd.to_datetime([1], unit='s')
+#         # expected values will have NaN where there were none
+#         expected_price = pd.Series([100.5], index=expected_index)
+#         expected_dividend = pd.Series([5.5], index=expected_index)
+#         # make assertions through the pandas testing module
+#         pd.testing.assert_series_equal(price, expected_price)
+#         pd.testing.assert_series_equal(dividend, expected_dividend)
+
+
+# class ShouldDownsampleUsingMeanValue(TestCase):
+#     def test(self):
+#         index = pd.to_datetime([1000, 2000], unit='ms')
+#         price = pd.Series([100., 101.], index=index)
+#         dividend = pd.Series([5., 6.], index=index)
+#         price, dividend = set_freq(price, dividend, '1T')
+#         print()
+#         print(price)
+#         print(dividend)
+#         # create the expected index and price
+#         expected_index = pd.to_datetime([1], unit='s')
+#         # expected values will have NaN where there were none
+#         expected_price = pd.Series([100.5], index=expected_index)
+#         expected_dividend = pd.Series([5.5], index=expected_index)
+#         # make assertions through the pandas testing module
+#         pd.testing.assert_series_equal(price, expected_price)
+#         pd.testing.assert_series_equal(dividend, expected_dividend)
